c4ab2315717384e080f5ea6fc7608c669ffd0df6
woescallada <94852222+woescallada@users.noreply.github.com>
2024-01-03 22:12:46 +0100

    Delete tarea_176 directory

--- tarea_176/2. Long_frase.py
+++
@@ -1,4 +0,0 @@
-'''Programa que diga la longitud de una frase'''
-s1 = str(input("Escribe una frase: "))
-
-print(len(s1))
--- tarea_176/5. palindromo.py
+++
@@ -1,6 +0,0 @@
-'''Programa que diga si una palabra dada es palíndromo'''
-pal = input (str("Escribe una palabra: "))
-if str(pal) == str(pal)[::-1]:
-    print ("La palabra ", pal, " es un palíndromo.")
-else:
-    print ("La palabra ", pal, " no es un palíndromo.")
